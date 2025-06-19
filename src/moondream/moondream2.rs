fab254ecf7a6ca5753261b1be576ff129e0bd317
zuramai <saugi.dev@gmail.com>
2025-06-19 07:03:12 +0700

    feat: detect function

--- src/moondream/moondream2.rs
+++ src/moondream/moondream2.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
-
+use imageproc;
 use half::f16;
 use image::DynamicImage;
 use ndarray::{s, Array1, Array2, Array3, Array6, ArrayD, ArrayViewD, Axis, Ix2, Ix3};
@@ -151,9 +151,10 @@ impl Moondream {
 
 
     fn run_decoder(&mut self, input_embeds: ArrayD<f16>, kv_cache: &mut ArrayD<f16> , pos: usize) -> Result<(ArrayD<f16>, ArrayD<f16>)>{
+        let kv_cache_slice = kv_cache.slice(s![.., .., .., .., ..pos, ..]).to_owned().into_dyn();
         let mut text_decoder  = self.text_decoder.run(HashMap::from([
-            ("input_embeds", input_embeds.as_standard_layout().view()),
-            ("kv_cache", kv_cache.as_standard_layout().view())
+            ("input_embeds", input_embeds.view()),
+            ("kv_cache", kv_cache_slice.view())
         ]), vec!["new_kv_cache", "hidden", "logits"])?;
         let kv_cache_update = text_decoder.remove("new_kv_cache").unwrap();
         let hidden = text_decoder.remove("hidden").unwrap();
@@ -167,10 +168,10 @@ impl Moondream {
     pub fn query(&self) -> String {
         "".into()
     }
-    pub fn detect(&mut self, image: DynamicImage, object: String) -> Result<String> {
+    pub fn detect(&mut self, image: DynamicImage, object: String) -> Result<Vec<types::Object>> {
         let prompt_toks = [
             self.config.templates.detect.prefix.as_slice(),
-            self.tokenizer.encode(object, false)?.get_ids(),
+            self.tokenizer.encode(format!(" {}", object), false)?.get_ids(),
             self.config.templates.detect.suffix.as_slice(),
         ].concat();
 
@@ -192,27 +193,78 @@ impl Moondream {
         let max_objects = 1;
 
         while objects.len() < max_objects {
-            let (logits, hidden_result) = self.run_decoder(hidden, &mut kv_cache, pos)?;
+            let (logits, hidden_result) = self.run_decoder(hidden.clone(), &mut kv_cache, pos)?;
             pos += hidden_result.shape()[hidden_result.ndim()-2];
+            dbg!(&logits.shape(), &logits.slice(s![0,..10]), util::argmax(&logits, -1), self.config.special_tokens.eos);
             if util::argmax(&logits, -1)[0] == self.config.special_tokens.eos {
                 break;
             }
 
+            // decode and encode x center coordinate
             let mut x_center = self.coord_decoder.run(HashMap::from([
                 ("input", hidden_result.slice(s![0, -1, ..]).view().into_dyn()),
-            ]), vec!["x_center"])?;
-            let x_center: ArrayD<f16> = x_center.remove("x_center").expect("`x_center` output does not exists");
-            let x_center = util::argmax(&x_center, -1) / x_center.shape()[x_center.ndim()-1];
-            let x_center = x_center.mapv(|v| v as f32);
+            ]), vec!["output"])?;
+            let x_center: ArrayD<f16> = x_center.remove("output").expect("`output` output does not exists");
+            let x_center_argmax = util::argmax(&x_center, -1);
+            let x_center_shape_last = x_center.shape()[x_center.ndim()-1];
+            let x_center = *x_center_argmax.first().unwrap() as f32 / x_center_shape_last as f32;
 
-            let mut hidden_result = self.coord_decoder.run(HashMap::from([
-                ("input", x_center.view().into_dyn()),
-            ]), vec!["hidden"])?;
-            hidden = hidden_result.remove("hidden").expect("`hidden` output does not exists");
+            let mut hidden_result = self.coord_encoder.run(HashMap::from([
+                ("input", Array1::from_vec(vec![f16::from_f32(x_center)]).into_dyn().view()),
+            ]), vec!["output"])?;
+            hidden = hidden_result.remove("output").expect("`output` does not exist");
+            hidden = hidden.insert_axis(Axis(0)).insert_axis(Axis(0));            
 
-            objects.push(x_center);
+            
+            // decode and encode y center coordinate
+            let (logits, hidden_result) = self.run_decoder(hidden, &mut kv_cache, pos)?;
+            pos += hidden_result.shape()[hidden_result.ndim()-2];
+            let mut y_infer = self.coord_decoder.run(HashMap::from([
+                ("input", hidden_result.slice(s![0, -1, ..]).view().into_dyn())
+            ]), vec!["output"])?;
+            let y_center: ArrayD<f16> = y_infer.remove("output").expect("`output` does not exists");
+            let y_center_argmax = util::argmax(&y_center, -1);
+            let y_center_shape_last = y_center.shape()[y_center.ndim()-1];
+            let y_center = *y_center_argmax.first().unwrap() as f32 / y_center_shape_last as f32;
+            
+            let mut hidden_result = self.coord_encoder.run(HashMap::from([
+                ("input", Array1::from_vec(vec![f16::from_f32(y_center)]).into_dyn().view()),
+            ]), vec!["output"])?;
+            hidden = hidden_result.remove("output").expect("`output` does not exist");
+            hidden = hidden.insert_axis(Axis(0)).insert_axis(Axis(0));
+            
+            // decode and encode size
+            let (logits, hidden_result) = self.run_decoder(hidden, &mut kv_cache, pos)?;
+            pos += hidden_result.shape()[hidden_result.ndim()-2];
+            let mut size_infer = self.size_decoder.run(HashMap::from([
+                ("input", hidden_result.slice(s![0, -1, ..]).view().into_dyn())
+            ]), vec!["output"])?;
+            let size: ArrayD<f16> = size_infer.remove("output").expect("`output` does not exists");
+            
+            let w_argmax = util::argmax(&size.slice(s![0, ..]).into_dyn().to_owned(), -1);
+            dbg!(&size.shape());
+            let w_argmax_val = *w_argmax.first().unwrap() as f32;
+            let size_last_dim = size.shape()[size.ndim()-1] as f32;
+            dbg!(&w_argmax_val, &size_last_dim);
+            let w = w_argmax_val / size_last_dim;
+            dbg!(&w);
+
+            let h_argmax = util::argmax(&size.slice(s![1, ..]).into_dyn().to_owned(), -1);
+            let h_argmax_val = *h_argmax.first().unwrap() as f32;
+            let h = h_argmax_val / size_last_dim;
+            dbg!(&h);
+
+            let wh = Array1::from_vec(vec![f16::from_f32(w), f16::from_f32(h)]);
+            
+            let mut hidden_result = self.size_encoder.run(HashMap::from([
+                ("input", wh.view().into_dyn()),
+            ]), vec!["output"])?;
+            hidden = hidden_result.remove("output").expect("`output` does not exist");
+            hidden = hidden.insert_axis(Axis(0)).insert_axis(Axis(0));
+
+            objects.push(types::Object::new(x_center,y_center,w,h));
         };
-        Ok("".into())
+        Ok(objects)
         
     }
     pub fn point(&self) -> String {
@@ -285,6 +337,9 @@ fn prepare_kv_cache(encoded_image: EncodedImage) -> ArrayD<f16> {
 
 #[cfg(test)]
 mod tests {
+    use image::{Pixel, Rgba};
+    use imageproc::{self, rect::Rect};
+
     use super::Moondream;
 
     // pub fn test_encode_image() {
@@ -305,9 +360,33 @@ mod tests {
     #[test]
     pub fn test_detect() {
         let mut md = Moondream::from_path("./model").expect("Failed to initialize moondream");
-        let img = image::open("demo-1.jpg").expect("Failed to open image demo-1.jpg");
-        let v = md.detect(img, "bread".to_string());
-        dbg!(&v);
-        assert!(v.is_ok());
+        let mut img = image::open("demo-4.jpeg").expect("Failed to open image demo-1.jpg");
+        let w = img.width();
+        let h = img.height();
+        let v = md.detect(img.clone(), "an id card".to_string());
+        if let Ok(detect_result) = v {
+            dbg!(&detect_result.len());
+            assert!(detect_result.len() > 0);
+
+            if detect_result.len() > 0 {
+                for object in detect_result {
+                    let scaled = object.scale(w as f32, h as f32);
+                    dbg!(&object);
+                    let x_center = scaled.x_center;
+                    let y_center = scaled.y_center;
+                    let x = scaled.x as i32;
+                    let y = scaled.y as i32;
+                    let w = scaled.w;
+                    let h = scaled.h;
+                    let radius = 5;
+                    imageproc::drawing::draw_filled_circle_mut(&mut img, (x_center.round() as i32, y_center.round() as i32), radius, Rgba([255, 0, 0, 255]));
+                    imageproc::drawing::draw_hollow_rect_mut(&mut img, Rect::at(x, y).of_size(w.round() as u32, h.round() as u32), Rgba([255, 0, 0, 255]));
+                }
+                img.save("demo-4-detect.png").expect("Failed to save image");
+                // new_img.save("demo-1-detect.png").expect("Failed to save image");
+            }
+        } else {
+            panic!("detect result is None");
+        }
     }
 }
--- src/moondream/types.rs
+++ src/moondream/types.rs
@@ -10,3 +10,36 @@ pub enum CaptionLength {
     Short,
     Normal,
 }
+
+#[derive(Debug)]
+pub struct Object {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+    pub x_center: f32,
+    pub y_center: f32,
+}
+
+impl Object {
+    pub fn new(x_center: f32, y_center: f32, w: f32, h: f32) -> Self {
+        Self {
+            x: x_center - w / 2.0,
+            y: y_center - h / 2.0,
+            w,
+            h,
+            x_center: x_center,
+            y_center: y_center,
+        }
+    }
+    pub fn scale(&self, scale_w: f32, scale_h: f32) -> Self {
+        Self {
+            x: self.x * scale_w,
+            y: self.y * scale_h,
+            w: self.w * scale_w,
+            h: self.h * scale_h,
+            x_center: self.x_center * scale_w,
+            y_center: self.y_center * scale_h,
+        }
+    }
+}
